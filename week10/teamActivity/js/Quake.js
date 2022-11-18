a7bac822c1b98de0bdd7e3afcb6386c34ae95a0d
victorlopez1995 <lop22010@byui.edu>
2022-11-17 20:59:21 -0500

    update week 10

---
+++ week10/teamActivity/js/Quake.js
@@ -0,0 +1,35 @@
+import { getJSON } from './utilities.js';
+// Quake Model
+export default class Quake {
+  constructor() {
+    this.baseUrl =
+      'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
+    // store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
+    this._quakes = [];
+  }
+  async getEarthQuakesByRadius(position, radius = 100) {
+    this._quakes = await getJSON(
+      this.baseUrl +
+        `&starttime=2019-01-01&endtime=2019-03-02&latitude=${
+          position.lat
+        }&longitude=${position.lon}&maxradiuskm=${radius}`
+    );
+    console.log(this._quakes)
+    return this._quakes;
+  }
+  getQuakeById(id) {
+    return this._quakes.features.filter(item => item.id === id)[0];
+  }
+}
+
+// function testGetQuakesForLocation() {
+//     let baseUrl = '';
+//     getLocation().then(response => {
+//         baseUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-03-02&latitude=${response.coords.latitude}&longitude=${response.coords.longitude}&maxradiuskm=100`;
+//         console.log(baseUrl);
+//         getJSON(baseUrl).then(response => {
+//             return response;
+//         })
+//     });
+
+// }
