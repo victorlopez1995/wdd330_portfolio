2e9ee369d0854eeb97fcb6bb8d750cd9591793e1
victorlopez1995 <lop22010@byui.edu>
2022-11-17 21:05:35 -0500

    add link week 10

--- js/main.js
+++ js/main.js
@@ -20,9 +20,11 @@ const links = [
       url: "week7/week7.html"
     },{label: "Week 8 notes",
     url: "week8/week8.html"
-  },{label: "Week 9 notes",
-  url: "week9/week9.html"
-}
+    },{label: "Week 9 notes",
+    url: "week9/week9.html"
+    },{label: "Week 10 notes",
+    url: "week10/week10.html"
+    }
   ]
 
 function weeklyList(array, idListName){
